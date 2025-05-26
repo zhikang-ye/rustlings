aa896e85445c3dcdf28d0b5fc8bbd7e56afb0ff1
yzk <867073116@qq.com>
2025-05-26 19:39:28 +0800

    record

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -11,6 +11,13 @@
 // TODO: Write a function that calculates the price of an order of apples given
 // the quantity bought.
 // fn calculate_price_of_apples(???) -> ??? { ??? }
+fn calculate_price_of_apples(num: u64) -> u64 {
+    if num > 40 {
+        num
+    } else {
+        num * 2
+    }
+}
 
 fn main() {
     // You can optionally experiment here.
