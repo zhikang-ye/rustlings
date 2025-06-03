a8a2faf09f63c24b688e7982d178b46bb44133e7
yzk <867073116@qq.com>
2025-06-03 20:34:43 +0800

    day5

--- exercises/13_error_handling/errors6.rs
+++ exercises/13_error_handling/errors6.rs
@@ -26,6 +26,9 @@ impl ParsePosNonzeroError {
 
     // TODO: Add another error conversion function here.
     // fn from_parse_int(???) -> Self { ??? }
+    fn from_parse_int(err: ParseIntError) -> Self {
+        Self::ParseInt(err)
+    }
 }
 
 #[derive(PartialEq, Debug)]
@@ -43,8 +46,10 @@ impl PositiveNonzeroInteger {
     fn parse(s: &str) -> Result<Self, ParsePosNonzeroError> {
         // TODO: change this to return an appropriate error instead of panicking
         // when `parse()` returns an error.
-        let x: i64 = s.parse().unwrap();
-        Self::new(x).map_err(ParsePosNonzeroError::from_creation)
+        match s.parse() {
+            Ok(x) => Self::new(x).map_err(ParsePosNonzeroError::from_creation),
+            Err(e) => Err(ParsePosNonzeroError::ParseInt(e)),
+        }
     }
 }
 
